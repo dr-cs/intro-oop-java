c4443b66d790d986facc13d4c82a3af65a242f11
Jay <jaygkamat@gmail.com>
2014-09-29 10:28:34 -0400

    Fix all errors.

--- code/employee/SalariedEmployee5.java
+++ code/employee/SalariedEmployee5.java
@@ -1,51 +1,51 @@
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
-public class SalariedEmployee5 extends Employee5 {
 
-    private static final int MONTHS_PER_YEAR = 12;
 
-    private final double annualSalary;
 
-    public SalariedEmployee5(String aName, Date aHireDate,
-                            double anAnnualSalary) {
-        super(aName, aHireDate);
-        disallowZeroesAndNegatives(anAnnualSalary);
-        annualSalary = anAnnualSalary;
-    }
 
-    @Override
-    public String getName() {
-        return "Salaried: " + super.getName();
-    }
 
-    public double getAnnualSalary() {
-        return annualSalary;
-    }
 
-    @Override
-    public double monthlyPay() {
-        return annualSalary / MONTHS_PER_YEAR;
-    }
 
-    public String toString() {
-        return super.toString() + "; Annual Salary: " + annualSalary;
-    }
 
-    public boolean equals(Object other) {
-        if (other == null) return false;
-        if (this == other) return true;
-        if (!(other instanceof SalariedEmployee3)) return false;
-        SalariedEmployee5 that = (SalariedEmployee5) other;
-        return super.equals(that) && (this.annualSalary == that.annualSalary);
-    }
 
-    public static void main(String[] args) throws Exception {
-        DateFormat df = DateFormat.getDateInstance();
-        SalariedEmployee5 eva = new SalariedEmployee5("Eva Luator",
-                                                      df.parse("June 10, 2013"),
-                                                      1000000);
-        System.out.println(eva.getName());
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- code/exceptions/MyException.java
+++ code/exceptions/MyException.java
@@ -1,6 +1,6 @@
-public class MyException extends Exception {
 
-    public MyException(String msg) {
-        super(msg);
-    }
-}
+
+
+
+
+

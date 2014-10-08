3ae5de5304964ca6e4745e795d281207e9e58860
Jay <jaygkamat@gmail.com>
2014-10-08 10:37:19 -0400

    You're spelling makes onions cri.

--- code/collections/ArrayListDemo.java
+++ code/collections/ArrayListDemo.java
@@ -32,7 +32,7 @@ public class ArrayListDemo {
         peeps.add(new LostPerson("Barney"));
 
         // 1:
-        // Will print true becuase the default implementation of equals
+        // Will print true because the default implementation of equals
         // uses == for object identity, and we're using the same reference
         // that's stored in peeps
         System.out.println("1: "+peeps.contains(wilma));
